7df29f023b9288ba979d369e4c8b4492336cd841
iwannadiehelpme <148798837+dudwo928@users.noreply.github.com>
2025-06-05 05:13:06 +0900

    BookMangaer 자바 파일에 임시로 (작동 여부는 잘 모름) 기능 실습용으로 구현 - C011039 김영재

--- se_test/src/book/BookManager.java
+++ se_test/src/book/BookManager.java
@@ -1,5 +1,27 @@
 package book;
 
-public class BookManager {
+import java.util.HashSet;
+import java.util.Set;
 
+public class BookManager
+{
+	// 젠킨스 자동화 실습 관련 책관리자 기능 일부 구현 - C011039 김영재
+	private Set<String> books = new HashSet<>();
+
+    // 책 추가
+    public void addBook(String title) {
+        if (books.contains(title)) {
+            throw new IllegalArgumentException("이미 존재하는 책입니다: " + title);
+        }
+        books.add(title);
+    }
+
+    // 책 제거
+    public void removeBook(String title) {
+        if (!books.contains(title)) {
+            throw new IllegalArgumentException("존재하지 않는 책입니다: " + title);
+        }
+        books.remove(title);
+    }
+	// C011039 작업 끝
 }
